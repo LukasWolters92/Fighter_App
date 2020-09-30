63f5501bc132d9849ba42342e411481dfe8f1cd9
Lukas Wolters <lukas.w8@SCHULE.KOELN>
2020-09-30 11:55:07 +0200

    Projektdateien hinzufügen.

---
+++ Fighter_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fighter_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
